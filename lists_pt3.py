79485bf3a5fbd3af02314e6727a2bcd0f138caeb
Austin Berry <aberry52098@gmail.com>
2023-10-10 15:36:51 -0400

    lists part 3 complete

---
+++ lists_pt3.py
@@ -0,0 +1,6 @@
+class_mates = ["Sam","Oliver","Val","David","Brandon","Mary","Austin"]
+if "Sean" in class_mates:
+    class_mates.remove("Sean")
+else:
+    class_mates.append("Sean")
+print(class_mates)
