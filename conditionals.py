c20c93d6b56a179ee417cee492f3626fc6ca44f4
Austin Berry <aberry52098@gmail.com>
2023-10-10 14:31:48 -0400

    change elif line 12 to else

--- conditionals.py
+++ conditionals.py
@@ -9,7 +9,7 @@ while True:
             magic_number = random.randrange(1, 10, 1)
             guess = int(input("Guess a number between 1 and 10\n"))
             continue
-        elif answer == "n":
+        else:
             break
     elif guess > 10 or guess < 1:
         guess = int(input("You are out of range. Guess again!\n"))
