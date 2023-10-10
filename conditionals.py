b54e49eabf0c1438fe9511ed4ffa85e4ead41891
Austin Berry <aberry52098@gmail.com>
2023-10-10 14:30:36 -0400

    added if logic to ask to play again

--- conditionals.py
+++ conditionals.py
@@ -1,11 +1,16 @@
 import random
 magic_number = random.randrange(1, 10, 1)
 guess = int(input("Guess a number between 1 and 10\n"))
-i = False
-while i == False:
+while True:
     if guess == magic_number:
-        print("Correct!!!")
-        i = True
+        print("Correct!!!\n")
+        answer = input("Would you like to play again? y or n: ")
+        if answer == "y":
+            magic_number = random.randrange(1, 10, 1)
+            guess = int(input("Guess a number between 1 and 10\n"))
+            continue
+        elif answer == "n":
+            break
     elif guess > 10 or guess < 1:
         guess = int(input("You are out of range. Guess again!\n"))
     elif guess != magic_number:
